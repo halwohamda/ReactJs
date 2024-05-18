a18e16e604c107f62d28458945037a2f1abc5bb7
halwohamda <halwohamda@gmail.com>
2024-05-18 15:55:54 -0800

    done

--- lesson/src/Footer.jsx
+++ lesson/src/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='footer'>&copy; halwo acadamy {new Date().getFullYear()}</div>
+   <footer>
+     <div className='footer'>&copy; halwo acadamy {new Date().getFullYear()}</div>
+   </footer>
   )
 }
 
