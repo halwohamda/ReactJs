e981a051d8cd248f016cd40a8203e432507fec4e
halwohamda <halwohamda@gmail.com>
2024-05-19 09:02:26 -0800

    done

--- lesson/src/App.jsx
+++ lesson/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header'
 import Footer from './Footer';
+import Food from './Food';
 
 const App = () => {
   return (
     <>
     <Header/>
     <Footer/>
+    <Food/>
     
     
     </>
