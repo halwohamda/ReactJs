813c18a6eba786e1d08c6ca5c66ab2518e34b247
halwohamda <halwohamda@gmail.com>
2024-05-19 10:30:16 -0800

    done

--- lesson2/src/Card.jsx
+++ lesson2/src/Card.jsx
@@ -4,7 +4,8 @@ import profile from '../public/profile.jpg'
 const Card = () => {
   return (
     <div>
-             <img src={profile} alt='profile picture' />
+             <img  src='http://via.placeholer.com/150 '  alt='profile picture'  />
+            
       <h1>Halwo </h1>
       <p>Full-stack developer FrontEnd backEnd react javascript </p>
     </div>
