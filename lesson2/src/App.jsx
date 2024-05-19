10d76f590febca530caecc3137a0049388411f3c
halwohamda <halwohamda@gmail.com>
2024-05-19 10:20:17 -0800

    done

--- lesson2/src/App.jsx
+++ lesson2/src/App.jsx
@@ -4,7 +4,7 @@ import profile from './public/profile.jpg'
 const App = () => {
   return (
     <div>
-      <img src={profile} alt={p} />
+      <img src={profile} alt='profile picture' />
       <h1>Halwo </h1>
       <p>Full-stack developer FrontEnd backEnd react javascript </p>
     </div>
