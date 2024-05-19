fd8548de1f82cb17a2f7843176e232eb74c38c73
halwohamda <halwohamda@gmail.com>
2024-05-19 10:22:15 -0800

    done

--- lesson2/src/App.jsx
+++ lesson2/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import profile from './public/profile.jpg'
+
 
 const App = () => {
   return (
     <div>
-      <img src={profile} alt='profile picture' />
-      <h1>Halwo </h1>
-      <p>Full-stack developer FrontEnd backEnd react javascript </p>
+ 
     </div>
   )
 }
--- lesson2/src/Card.jsx
+++ lesson2/src/Card.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import profile from './public/profile.jpg'
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <div>
+             <img src={profile} alt='profile picture' />
+      <h1>Halwo </h1>
+      <p>Full-stack developer FrontEnd backEnd react javascript </p>
+    </div>
   )
 }
 
