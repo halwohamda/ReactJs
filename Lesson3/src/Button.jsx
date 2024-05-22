14ddb43f26f7b5c3801aa23ad257c612fb90ab83
halwohamda <halwohamda@gmail.com>
2024-05-21 19:44:57 -0800

    done

--- Lesson3/src/Button.jsx
+++ Lesson3/src/Button.jsx
@@ -1,21 +1,21 @@
 
 
-// const Button = () => {
-//    const style={
-//     backgroundColor: "rgb(76, 76, 234)",
-//     color: "white",
-//     padding: "10px 20px",
-//     outline: "none",
-//     margin: "20",
-//     borderRadius: "10px",
-//     cursor: "pointer",
-//    }
+const Button = () => {
+   const style={
+    backgroundColor: "rgb(76, 76, 234)",
+    color: "white",
+    padding: "10px 20px",
+    outline: "none",
+    margin: "20",
+    borderRadius: "10px",
+    cursor: "pointer",
+   }
 
-//   return (
+  return (
 
-//     <button className="button">ClickMe</button>
+    <button className="button">ClickMe</button>
   
-//   )
-// }
+  )
+}
 
-// export default Button
+export default Button
