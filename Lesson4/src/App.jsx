de399f9590290f9900d7606edc6a3291fb6050eb
halwohamda <halwohamda@gmail.com>
2024-05-25 10:50:06 -0800

    done

--- Lesson4/src/App.jsx
+++ Lesson4/src/App.jsx
@@ -3,7 +3,7 @@ import Students from './Students.jsx'
 const App = () => {
   return (
     <div>
-        <Students name="mohamed"/>
+        <Students name="mohamed" age="20" section="13b"/>
     </div>
   )
 }
